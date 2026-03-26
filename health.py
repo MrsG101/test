4260400edda087ad57f91dac9767bce158a807b5
MrsG101 <d.galsan1@gmail.com>
2026-03-27 00:01:49 +0800

    Update health.py

--- health.py
+++ health.py
@@ -10,3 +10,39 @@ st.caption("""
 """)
 
 st.info("Зүүн талын menu-ээс судалгааны хуудсыг сонгоно уу.")
+def global_filter(df, st):
+    st.sidebar.header("GLOBAL FILTER")
+
+    death = st.sidebar.multiselect(
+        "Нас барсан эсэх",
+        df["Нас барсан эсэх"].dropna().unique()
+    )
+
+    year = st.sidebar.multiselect(
+        "Он",
+        sorted(df["year"].dropna().unique())
+    )
+
+    month = st.sidebar.multiselect(
+        "Сар",
+        sorted(df["month"].dropna().unique())
+    )
+
+    soum = st.sidebar.multiselect(
+        "SOUM",
+        df["SOUM"].dropna().unique()
+    )
+
+    if death:
+        df = df[df["Нас барсан эсэх"].isin(death)]
+
+    if year:
+        df = df[df["year"].isin(year)]
+
+    if month:
+        df = df[df["month"].isin(month)]
+
+    if soum:
+        df = df[df["SOUM"].isin(soum)]
+
+    return df
